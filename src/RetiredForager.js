6bbd96f88796f871085e60dc57622b96e452086f
Simeon Lee <simeonhlee@gmail.com>
2016-08-01 11:38:35 -0700

    Implement subclass functionality in all files

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this); // creates subclass of ForagerBee
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype); // meant to have prototype lookup fall through to superclass prototype
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;  // meant to have constructor delegate to RetiredForagerBee instead of ForagerBee
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+}
